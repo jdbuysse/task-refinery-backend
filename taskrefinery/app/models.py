bb5adeec5939c29aa1741c570cc83ed34d4ecf13
jdbuysse <jdbuysse@gmail.com>
2020-06-24 12:00:31 -0600

    created first model and serializers

---
+++ taskrefinery/app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    description = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['created']
+
