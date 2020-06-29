6a4f65783d2d70647c17c81edaa9fab1ca020926
jdbuysse <jdbuysse@gmail.com>
2020-06-29 11:19:36 -0600

    through part 2 of tutorial

--- taskrefinery/app/urls.py
+++ taskrefinery/app/urls.py
@@ -1,7 +1,18 @@
+# from django.urls import path
+# from app import views
+
+# urlpatterns = [
+#     path('tasks/', views.task_list),
+#     path('tasks/<int:pk>/', views.task_detail),
+# ]
+
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from app import views
 
 urlpatterns = [
     path('tasks/', views.task_list),
-    path('tasks/<int:pk>/', views.task_detail),
+    path('tasks/<int:pk>', views.task_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
