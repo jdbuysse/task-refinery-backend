b5b2d4e8382f282fd9186148722239f3d124c17a
jdbuysse <jdbuysse@gmail.com>
2020-06-30 15:29:24 -0600

    finished video tutorial, I think things are working

--- taskrefinery/app/serializers.py
+++ taskrefinery/app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from . import models
+from django.contrib.auth.hashers import make_password
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,4 +11,14 @@ class TaskSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'password', 'email')
+        extra_kwargs = { 'password': {'write_only': True }}
+
+    def create(self, validated_data):
+        user = models.User.objects.create(
+            email = validated_data['email'],
+            username = validated_data['username'],
+            password = make_password(validated_data['password'])
+        )
+        user.save()
+        return user
